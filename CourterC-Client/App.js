83371a3bbb4f6a319e379f0c714600c440cca8ed
grahagandangr <grahagandangr@gmail.com>
2022-09-07 23:04:16 +0700

    add courtCard

--- CourterC-Client/App.js
+++ CourterC-Client/App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons'; 
-import tw from "twrnc"
-import Home from "./pages/HomePage"
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import tw from "twrnc";
+import HomePage from "./screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,7 +12,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} 
+        <Tab.Screen
+          name="Home"
+          component={HomePage}
           options={{
             headerShown: false,
             tabBarShowLabel: false,
@@ -20,8 +22,9 @@ export default function App() {
               <View style={tw`items-center justify-center content-center`}>
                 <Ionicons name="home-outline" size={19} color="black" />
               </View>
-            )
-          }}/>
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- CourterC-Client/tailwind.config.js
+++ CourterC-Client/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./scrrens/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
