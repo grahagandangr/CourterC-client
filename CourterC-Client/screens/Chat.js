54949db15543629ba695a1c9718cbb131fc2d92f
IwanWinata <untuksekolah12688@gmail.com>
2022-09-10 22:58:06 +0700

    feat: chatpage

--- CourterC-Client/screens/Chat.js
+++ CourterC-Client/screens/Chat.js
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+import tw from "twrnc";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ChatCard from "../components/ChatCard";
 
 export default function Chat() {
   return (
-    <View>
-      <Text>Chat</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <View
+        style={tw`bg-blue-600 w-full h-10 rounded-b-3xl opacity-85 px-8 flex flex-row`}
+      >
+        <Text style={tw`m-auto text-base text-white font-semibold`}>Chat</Text>
+      </View>
+      <ScrollView>
+        <ChatCard />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
